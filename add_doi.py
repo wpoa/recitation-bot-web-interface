ad150e5f2056f34b93ce7001b8db7c88c4489650
notconfusing <isalix@gmail.com>
2014-09-30 19:59:47 +0000

    image and text checkboxes

--- add_doi.py
+++ add_doi.py
@@ -15,46 +15,57 @@ print "<body>"
 print "<form method='post'>"
 print "Enter a doi (don't include 'http://dx.doi.org/'):"
 print "<input type='text' name='doi' value='' /><BR/>"
-print "<input type='checkbox' name='reupload' />Force reupload if doi already uploaded<BR/>"
+print "If DOI already uploaded, force reupload of <input type='checkbox' name='reupload_text' />text"
+print "<input type='checkbox' name='reupload_images' />images.<br/>"
 print "<input type='submit' value='submit form' />"
 print "</form>"
 form = cgi.FieldStorage()
 
-reupload = 'reupload_off'
+reupload = []
+doi_plain = ''
+
 #print form
 for field_name in form:
     field=form[field_name]
-    if field.name == 'reupload':
-        if form['reupload'].value == 'on':
-            print "<p>reupload: %s</p>" % form['reupload'].value
-            reupload = 'reupload_on'
+    if field.name == 'reupload_text':
+        if form['reupload_text'].value == 'on':
+            print "<p>reupload_text: %s</p>" % form['reupload_text'].value
+            reupload.append('reupload_text')
+    if field.name == 'reupload_images':
+        if form['reupload_images'].value == 'on':
+            print "<p>reupload_images: %s</p>" % form['reupload_images'].value
+            reupload.append('reupload_images')
     if field.name == 'doi':
         doi_plain = field.value
         doi_safe = cgi.escape(repr(doi_plain))
-        print "<p>%s will be uploaded shortly</p>" % doi_safe
-        url = 'http://tools.wmflabs.org/recitation-bot/' + doi_plain + '.html'
-        print "<p>Follow the upload status at <a href='%s'>%s</a> </p>" % (url, url)
-        if form['doi'].value:
-            dequeue = open('/data/project/recitation-bot/recitation-bot/jump_the_queue.log','a')
-            dequeue.write(doi_plain+'\t'+reupload+'\n')
-            dequeue.close()
+
+if doi_plain:
+    #write to log
+    dequeue = open('/data/project/recitation-bot/recitation-bot/jump_the_queue.log','a')
+    dequeue.write(doi_plain+'\t'+str(reupload)+'\n')
+    dequeue.close()
     
-        waiting_page_path = '/data/project/recitation-bot/public_html/'+doi_plain+'.html'
-        if not os.path.exists(os.path.dirname(waiting_page_path)):
-            os.makedirs(os.path.dirname(waiting_page_path))
-        if os.path.isfile(waiting_page_path):
-            pass #there's nothing to do since its already been uplaoded
-        else:
-            waiting_page = open(waiting_page_path, 'w')
-            waiting_text = r'''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+    #show the user
+    print "<p>%s will be uploaded shortly</p>" % doi_safe
+    url = 'http://tools.wmflabs.org/recitation-bot/' + doi_plain + '.html'
+    print "<p>Follow the upload status at <a href='%s'>%s</a> </p>" % (url, url)
+    #make the waiting page
+    waiting_page_path = '/data/project/recitation-bot/public_html/'+doi_plain+'.html'
+    if not os.path.exists(os.path.dirname(waiting_page_path)):
+        os.makedirs(os.path.dirname(waiting_page_path))
+    if os.path.isfile(waiting_page_path):
+        pass #there's nothing to do since its already been uplaoded
+    else:
+        waiting_page = open(waiting_page_path, 'w')
+        waiting_text = r'''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <title>waiting</title>
 <html>
 <body>
 <p>%s being processed, we have to fetch and crunch a lot of data, long articles can take upto 5 minutes depending on traffic loads</p>
 </body>
 </html>''' % doi_plain
-            waiting_page.write(waiting_text)
-            waiting_page.close()
+        waiting_page.write(waiting_text)
+        waiting_page.close()
 
 
 print "</ul>"
